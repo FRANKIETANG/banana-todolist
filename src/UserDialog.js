270a9a6cc93c639b3d505a1cb5a27e919a6f86ec
FRANKIETANG <350558468@qq.com>
2017-08-23 17:56:43 +0800

    UserDialog.css

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react'
+import './UserDialog.css'
 
 export default class UserDialog extends Component{
     render(){
         return(
             <div className="UserDialog-Wrapper">
-                <div>
+                <div className="UserDialog">
                     <nav>
                         <input type="radio"/> 注册
                         <input type="radio"/> 登录
                     </nav>
-                    <div>
+                    <div className="panes">
                         <form className="signUp"> {/* 注册*/}
                             <div className="row">
                                 <label>用户名</label>
                                 <input type="text"/>
                             </div>
-                            <div>
+                            <div className="row">
                                 <label>密码</label>
                                 <input type="password"/>
                             </div>
