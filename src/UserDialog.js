f02bef5005f12d903d445b458cacd17c76be4e8b
FRANKIETANG <350558468@qq.com>
2017-08-24 20:43:18 +0800

    添加返回登录按钮

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -163,6 +163,7 @@ export default class UserDialog extends Component{
                     </div>
                     <div className="row actions">
                         <button type="submit">发送重置邮件</button>
+                        <a href="#" onClick={this.returnToSignIn.bind(this)}>返回登录</a>
                     </div>
                 </form>
             </div>
@@ -180,6 +181,11 @@ export default class UserDialog extends Component{
     showForgotPassword(){
         let stateCopy = JSON.parse(JSON.stringify(this.state))
         stateCopy.selectedTeb = 'forgotPassword'
+        this.setState(stateCopy)
+    }
+    returnToSignIn(){
+        let stateCopy = JSON.parse(JSON.stringify(this.state))
+        stateCopy.selectedTeb = 'signInOrSignUp'
         this.setState(stateCopy)
     }
     resetPassword(e){
