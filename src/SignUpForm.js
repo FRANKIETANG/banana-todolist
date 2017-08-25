8112ca53992598d6ad5ab405692c4a105eb739ab
FRANKIETANG <350558468@qq.com>
2017-08-25 14:49:19 +0800

    修改注册和登录模块

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -1,26 +1,28 @@
+import React from 'react'
+
 export default function (props) {
     return(
         <form className="signUp" 
-        onSubmit={this.props.onSubmit.bind(this)}> {/* 注册*/}
+        onSubmit={props.onSubmit}> {/* 注册*/}
             <div className="row">
                 <label>邮箱</label>
                 <input type="text" 
-                value={this.props.formData.email}
-                onChange={this.props.onChange.bind(null,'email')}
+                value={props.formData.email}
+                onChange={props.onChange.bind(null,'email')}
                 />
             </div>
             <div className="row">
                 <label>用户名</label>
                 <input type="text" 
-                value={this.props.formData.username} 
-                onChange={this.props.onChange.bind(null,'username')}/>
+                value={props.formData.username} 
+                onChange={props.onChange.bind(null,'username')}/>
                 {/* bind 不仅可以绑定 this，还可以绑定第一个参数 */}
             </div>
             <div className="row">
                 <label>密码</label>
                 <input type="password" 
-                value={this.props.formData.password} 
-                onChange={this.props.onChange.bind(null,'password')}/>
+                value={props.formData.password} 
+                onChange={props.onChange.bind(null,'password')}/>
             </div>
             <div className="row actions">
                 <button type="submit">注册</button>
